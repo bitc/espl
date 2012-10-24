cf49290b5b3230de44725a05f71dd7b32f6c61e6
Benjamin Kramek <kramekb@post.bgu.ac.il>
2012-10-24 18:28:59 +0200

    lab01/barcode: Implement main function in encbc.c

--- lab01/barcode/encbc.c
+++ lab01/barcode/encbc.c
@@ -3,7 +3,41 @@
 #include "bclib.h"
 
 int main(int argc, char **argv) {
-	// TODO ...
+	int barcode[BC_WIDTH][BC_NBITS];
+	int i, j;
+	int n;
+	char* c;
+
+	n = 0;
+	for(i=1; i<argc; ++i) {
+		c = argv[i];
+		while(*c != '\0') {
+			bc_char2bits(*c, barcode[n]);
+			++n;
+			++c;
+		}
+		// Add space character between words
+		if(i!=argc-1) {
+			bc_char2bits(' ', barcode[n]);
+			++n;
+		}
+		if(n == BC_WIDTH) {
+			// Don't overflow
+			break;
+		}
+	}
+
+	for(j=0; j<BC_NBITS; ++j) {
+		for(i=0; i<n; ++i) {
+			if(barcode[i][j]) {
+				printf("#");
+			}
+			else {
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
