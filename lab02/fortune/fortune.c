9c4aa7655f65255f97d67267c2a6c9a7a7e85ddb
Benjamin Kramek <kramekb@post.bgu.ac.il>
2012-10-31 16:29:21 +0200

    lab02/fortune: Add random_number function and test it

--- lab02/fortune/fortune.c
+++ lab02/fortune/fortune.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define MAX_LINE_LENGTH 128
  
 long get_lines(FILE* fp);
+long random_number(long max);
 
 /* print random line */
 int main(int argc, char **argv) {
@@ -22,8 +25,12 @@ int main(int argc, char **argv) {
 	/* TODO This print is only temporary for testing */
 	printf("num lines in file: %lu\n", num_lines);
 
-	/* TODO Finish */
 
+	long random_line = random_number(num_lines);
+
+	printf("random line: %lu\n", random_line);
+
+	/* TODO Finish */
 	fclose(fp);
  
 	return 0;
@@ -40,4 +47,11 @@ long get_lines(FILE* fp) {
 	}
 
 	return numlines;
+}
+
+/* Returns a random number between 0 and max (inclusive) */
+long random_number(long max) {
+	srandom(time(0));
+
+	return random() % (max+1);
 }
