d629270242ff0f4baf2e4fa65abc05d8805869b1
Benjamin Kramek <kramekb@post.bgu.ac.il>
2012-10-31 17:15:01 +0200

    lab02/canalyze: Print all the names that appear in the files

--- lab02/canalyze/canalyze.c
+++ lab02/canalyze/canalyze.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "namelist.h"
+
+void read_file(char *fname, struct namelist *namelist);
 
 int main(int argc, char **argv) {
+	int i;
+
+	struct namelist *namelist;
+
+	namelist = make_namelist();
+
+	for(i = 1; i < argc; ++i) {
+		read_file(argv[i], namelist);
+	}
 	/* TODO */
 
+	free(namelist->names);
+	free(namelist);
+
 	return 0;
+}
+
+void read_file(char *fname, struct namelist *namelist) {
+	FILE *fp;
+	char name[NAMELEN];
+
+	printf("%s\n", fname);
+
+	fp = fopen(fname, "r");
+	if(!fp) {
+		perror("Error opening file");
+		return;
+	}
+
+	while(fgetname(name, NAMELEN, fp)) {
+		printf("%s ", name);
+	}
+	printf("\n");
+
+	fclose(fp);
 }
