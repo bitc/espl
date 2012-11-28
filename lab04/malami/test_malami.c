e2c835f807278e604eb71a1d4d59bb4cf7063cc1
Benjamin Kramek <kramekb@post.bgu.ac.il>
2012-11-28 15:09:33 +0200

    Add a simple program that demonstrates the use of the malami function

--- lab04/malami/test_malami.c
+++ lab04/malami/test_malami.c
@@ -1,10 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/*TODO: add signature for malami */
+/* malami is a like the atoi function from the standard library */
+int malami(char* str);
 
 int main(int argc, char **argv) {
-	/* TODO: call malami */
+	char* test_string = "5123";
+
+	if(argc != 2) {
+		printf("Usage: %s [NUMBER]\n", argv[0]);
+		return -1;
+	}
+
+	printf("Testing malami with string %s, result is: %d\n", test_string, malami(test_string));
+	printf("Number from command line argument is: %d\n", malami(argv[1]));
 
 	return 0;
 }
