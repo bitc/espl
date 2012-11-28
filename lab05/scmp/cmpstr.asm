d7e5944e876792eb35756c62c6c23530469bf28b
Benjamin Kramek <kramekb@post.bgu.ac.il>
2012-11-28 18:01:45 +0200

    lab05/scmp: Add asm implementation of cmpstr

--- lab05/scmp/cmpstr.asm
+++ lab05/scmp/cmpstr.asm
@@ -2,9 +2,37 @@ section .text
 	global cmpstr
 cmpstr:
 	push ebp
+	mov ebp, esp
 
-	; TODO
+loop:
+	mov eax, DWORD [ebp+8]
+	movzx eax, BYTE [eax]
+	mov ebx, DWORD [ebp+12]
+	movzx ebx, BYTE [ebx]
+	cmp eax, 0
+	je test
+	cmp eax, ebx
+	jne test
+	inc DWORD [ebp+8]
+	inc DWORD [ebp+12]
+	jmp loop
 
+test:
+	cmp eax, ebx
+	jl less
+	jg greater
+
+equals:
 	mov eax, 0
+	pop ebp
+	ret
+
+greater:
+	mov eax, 1
+	pop ebp
+	ret
+
+less:
+	mov eax, 2
 	pop ebp
 	ret
